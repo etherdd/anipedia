2d0d5fea6f1724bec62fcbb73f61be399898cd77
Yufei Chen <yuphymiachin@yahoo.com>
2024-03-10 11:13:38 -0700

    Cleanup

--- webapp/app/animation/[id]/page.js
+++
@@ -1,4 +0,0 @@
-
-export default function Animation({ params }) {
-  return <h1>Animation {params.id}</h1>
-}
--- webapp/app/api/animation/[id]/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-// Get information of a specific animation.
-export async function GET(request, {params}) {
-  return NextResponse.json({title: "Wind", id: params.id});
-}
