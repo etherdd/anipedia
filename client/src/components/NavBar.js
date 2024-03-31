e016df0ed4a25aa9445710161c092ce8d031367c
Yufei Chen <yuphymiachin@yahoo.com>
2024-03-30 23:34:21 -0700

    Create Navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,47 +1,59 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material'
 import { NavLink } from 'react-router-dom';
+import logo from './logo.png';
 
+import './NavBar.css';
 // The hyperlinks in the NavBar contain a lot of repeated formatting code so a
 // helper component NavText local to the file is defined to prevent repeated code.
-function NavText({ href, text, isMain }) {
-  return (
-    <Typography
-      variant={isMain ? 'h5' : 'h7'}
-      noWrap
-      style={{
-        marginRight: '30px',
-        fontFamily: 'monospace',
-        fontWeight: 700,
-        letterSpacing: '.3rem',
-      }}
-    >
-      <NavLink
-        to={href}
-        style={{
-          color: 'inherit',
-          textDecoration: 'none',
-        }}
-      >
-        {text}
-      </NavLink>
-    </Typography>
-  )
-}
+// function NavText({ href, text, isMain }) {
+//   return (
+//     <Typography
+//       noWrap
+//       style={{
+//         marginRight: '30px',
+//         fontFamily: 'monospace',
+//         fontWeight: 700,
+//         letterSpacing: '.3rem',
+//       }}
+//     >
+//       <NavLink
+//         to={href}
+//         style={{
+//           color: 'inherit',
+//           textDecoration: 'none',
+//         }}
+//       >
+//         {text}
+//       </NavLink>
+//     </Typography>
+//   )
+// }
 
 // Here, we define the NavBar. Note that we heavily leverage MUI components
 // to make the component look nice. Feel free to try changing the formatting
 // props to how it changes the look of the component.
 export default function NavBar() {
   return (
-    <AppBar position='static' style={{ backgroundColor: '#000000' }}>
-      <Container maxWidth='xl'>
+    <div className="navbar">
+      <img src={logo} className="logo" alt='logo'></img>
+
+      <ul>
+        <li><a href='/' text='HOME' >Home</a></li>
+        <li><a href='/toppicks' >Top Picks</a></li>
+        <li><a href='/directors' >Directors</a></li>
+        <li><a href='/search' >Search</a></li>
+      </ul>
+
+      <div className='empty-div'></div>
+
+      {/* <Container maxWidth='xl'>
         <Toolbar disableGutters>
           <NavText href='/' text='AniPedia' isMain />
           <NavText href='/search' text='search' />
           <NavText href='/top_movie' text='top movies' />
           <NavText href='/top_person' text='top talents' />
         </Toolbar>
-      </Container>
-    </AppBar>
+      </Container> */}
+
+    </div>
   );
 }
