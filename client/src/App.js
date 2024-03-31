2a39b88bf49b4e285e45c50196e9fe5a532b952d
Yufei Chen <yuphymiachin@yahoo.com>
2024-03-30 22:35:34 -0700

    Create subpages

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,11 @@ import TopTalentPage from './pages/TopTalentPage';
 import SearchPage from './pages/SearchPage';
 import PersonInfoPage from './pages/PersonInfoPage'
 import MovieInfoPage from './pages/MovieInfoPage'
-
+import SearchPage from './pages/SearchPage';
+import TopPicksPage from './pages/TopPicksPage';
+import TopDirectorsPage from './pages/TopDirectorsPage';
+import AnimationInfoPage from './pages/AnimationInfoPage';
+import DirectorInfoPage from './pages/DirectorInfoPage';
 
 // createTheme enables you to customize the look and feel of your app past the default
 // in this case, we only change the color scheme
@@ -35,10 +39,14 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/top_movie" element={<TopMoviePage />} />
+          {/* <Route path="/top_movie" element={<TopMoviePage />} />
           <Route path="/top_person" element={<TopTalentPage />} />
           <Route path="/movie/:movie_id" element={<MovieInfoPage />} />
-          <Route path="/person/:person_id" element={<PersonInfoPage />} />
+          <Route path="/person/:person_id" element={<PersonInfoPage />} /> */}
+          <Route path="/toppicks" element={<TopPicksPage />} />
+          <Route path="/directors" element={<TopDirectorsPage />} />
+          <Route path="/animation/:animation_id" element={<AnimationInfoPage />} />
+          <Route path="/director/:director_id" element={<DirectorInfoPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
