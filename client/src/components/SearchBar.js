0dbd517440a0f0119cce65799df6ed728795145a
Yun Dai <yundai@seas.upenn.edu>
2024-04-10 18:33:42 +0200

    Home page UI update

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,43 +1,18 @@
 import React from 'react';
-import { TextField, Button, Box } from '@mui/material';
+import { TextField, Button, Grid } from '@mui/material';
 
 const SearchComponent = () => {
+  const title = "";
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '50px', // Applied margin as requested
-      }}
-    >
-      <TextField
-        sx={{
-          background: 'rgba(255, 255, 255, 0.5)', // White background with 50% transparency
-          //maxWidth: '300px',
-          '& .MuiOutlinedInput-root': {
-            borderRadius: '4px 0 0 4px', // Rounded corners on the left side only
-            height: '56px', // Standard height to align with the Button
-          }
-        }}
-        variant="outlined"
-        fullWidth // Make TextField responsive within the Box constraints
-      />
-      <Button
-        sx={{
-          backgroundColor: 'black',
-          color: 'white',
-          '&:hover': {
-            backgroundColor: '#333', // Slightly darker on hover
-          },
-          height: '56px', // Ensuring the Button height matches the TextField
-          width: '100px', // Ensuring the Button height matches the TextField
-          borderRadius: '0 4px 4px 0', // Rounded corners on the right side only
-        }}
-      >
-        Search
-      </Button>
-    </Box>
+    <Grid container spacing={2} style={{ padding: '100px', paddingTop: '30px', paddingBottom: '30px'}}>
+    <Grid item xs={10}>
+      <TextField label='Title' value={title} style={{ width: "100%", height: '56px' }}/>
+    </Grid>
+    <Grid item xs={2}>
+      <Button  style={{ color:'white', background: 'grey', height: '56px', width: '100%'}}>Search</Button>
+    </Grid>
+  </Grid>
   );
 };
 
