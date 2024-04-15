33d735342fe2720a0439d51080a390e410ccb738
Yufei Chen <yuphymiachin@yahoo.com>
2024-04-15 00:32:17 -0700

    Add Google and Github login using Auth0

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrap App (root component) with Auth0Provider */}
+    <Auth0Provider
+      domain="dev-gqlnlgwrauklcpmj.us.auth0.com"
+      clientId="vurbavL3ppsxSW6ELUA4mwedlFRs6O1D"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
